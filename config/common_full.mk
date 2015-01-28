95f95cbb21b9d9446b35664f82185cbdad9e3df7
Michael Bestas <mikeioannina@gmail.com>
2015-01-28 02:04:48 +0200

    Remove deprecated VideoEditor packages
    Change-Id: I47b1d17e6b93dae6992fe169d034bfa4eb820370
    
    Conflicts:
    	config/common_full.mk

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,9 +1,6 @@
 # Inherit common SOKP stuff
 $(call inherit-product, vendor/sokp/config/common.mk)
 
-# Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
-
 # Include SOKP audio files
 include vendor/sokp/config/cm_audio.mk
 
